4678eb849e88dd6cab7dfbe97b3445be0258d263
Nobobi-Hasan <shatin.hasan73@gmail.com>
2023-02-16 12:24:49 +0600

    bootstrap added

--- src/main.js
+++ src/main.js
@@ -2,10 +2,17 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 
-import './assets/main.css'
+
+/** Custom CSS link */
+import './assets/sass/style.scss'
+
+
 
 const app = createApp(App)
 
 app.use(createPinia())
 
 app.mount('#app')
+
+/** import All of Bootstrap JS */
+import * as bootstrap from 'bootstrap';
