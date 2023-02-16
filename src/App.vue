a96f5459545288e0ea5ef5393eaf8e6935d63951
Nobobi-Hasan <shatin.hasan73@gmail.com>
2023-02-16 12:57:32 +0600

    UI design and axios install

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,50 @@
 <script setup>
-
 </script>
 
 <template>
-  <h1 class="text-bg-danger">Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores, optio sunt est accusamus suscipit quia corporis voluptas atque adipisci, ad totam fugit quos alias rerum molestias amet laboriosam magni quas.</h1>
+  <div class="container">
+    <div class="row my-5">
+      <div class="col-md-12">
+        <div class="card">
+          <div class="card-body">
+            <h4 class="card-title text-center"> Task List</h4>
 
+            <div class="row mb-3">
+
+              <form action="">
+                <div class="col-md-12">
+                  <label for="" class="form-label">Add New Task</label>
+                  <input type="text" class="form-control">
+                </div>
+
+                <div class="mt-3">
+                  <input type="button" name="" id="" class="btn btn-success" value="Add Task">
+                </div>
+
+                
+              </form>
+
+            </div>
+          </div>
+
+          <div class="card-body">
+            <h4 class="text-danger">Tasks</h4>
+            
+            <ul class="list-group">
+              <li class="list-group-item d-flex justify-content-between align-items-center">
+                List Item
+                <a href="" class="btn ">
+                  <i class="fa-solid fa-xmark"></i>
+                </a>
+              </li>
+            </ul>
+
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  
 </template>
 
 <style scoped>
